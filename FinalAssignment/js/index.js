27f7892161d614f10d309aa5fe259b8d92ee548a
sim-my <simranpanthi101@gmail.com>
2021-01-14 17:56:36 +0545

    Created Intendation in code

--- FinalAssignment/js/index.js
+++ FinalAssignment/js/index.js
@@ -1,25 +1,27 @@
 document.querySelector('.search-field').style.display = 'none';
-document
-  .querySelector(".mobile-navigation-menu")
-  .addEventListener("click", openMobileNavigation);
+
+document.querySelector(".search-bar-icon").addEventListener("click", handleSearchBar);
+
+document.querySelector(".mobile-navigation-menu").addEventListener("click", openMobileNavigation);
+
+document.querySelector(".mobile-menu-header").addEventListener("click", closeMobileNavigation);
+
+document.querySelector(".search-bar-icon").addEventListener("click", handleSearchBar);
 
 function openMobileNavigation() {
+
   document.querySelector(".mobile-menu").style.display = "block";
   document.querySelector("body").style.overflow = "hidden";
-}
 
-document
-  .querySelector(".mobile-menu-header")
-  .addEventListener("click", closeMobileNavigation);
+}
 
 function closeMobileNavigation() {
+
   document.querySelector(".mobile-menu").style.display = "none";
   document.querySelector("body").style.overflow = "auto";
+
 }
 
-document
-  .querySelector(".search-bar-icon")
-  .addEventListener("click", handleSearchBar);
 function handleSearchBar() {
 
   if(document.querySelector(".search-field").style.display == "none"){
@@ -35,14 +37,14 @@ function handleSearchBar() {
     document.querySelector(".search-bar-icon img").style.height = '30px';
     document.querySelector(".search-bar-icon img").style.width = '30px';
     document.querySelector(".search-bar-icon").style.top = '43px';
-  }
-  
+  }  
+
 }
 
 var slideIndex = 1;
+
 showSlides(slideIndex);
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides((slideIndex = n));
 }
@@ -51,23 +53,25 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("slide");
   var dots = document.getElementsByClassName("dot");
+
   if (n > slides.length) {
     slideIndex = 1;
   }
+
   if (n < 1) {
     slideIndex = slides.length;
   }
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
+
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
-  setTimeout(showSlides, 2000);
 }
-document
-  .querySelector(".search-bar-icon")
-  .addEventListener("click", handleSearchBar);
+
 
