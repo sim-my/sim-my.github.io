4f3e2954d62c27cb2a03b31c5c00d9efdb01e524
sim-my <simranpanthi101@gmail.com>
2021-01-14 11:24:03 +0545

    Added some changes in designs

--- FinalAssignment/js/index.js
+++ FinalAssignment/js/index.js
@@ -1,39 +1,67 @@
-document.querySelector('.mobile-navigation-menu').addEventListener("click", openMobileNavigation);
+document.querySelector('.search-field').style.display = 'none';
+document
+  .querySelector(".mobile-navigation-menu")
+  .addEventListener("click", openMobileNavigation);
 
-function openMobileNavigation(){
-    document.querySelector('.mobile-menu').style.display = 'block';
-    document.querySelector('body').style.overflow = 'hidden';
-
+function openMobileNavigation() {
+  document.querySelector(".mobile-menu").style.display = "block";
+  document.querySelector("body").style.overflow = "hidden";
 }
 
-document.querySelector('.mobile-menu-header').addEventListener("click", closeMobileNavigation);
+document
+  .querySelector(".mobile-menu-header")
+  .addEventListener("click", closeMobileNavigation);
 
-function closeMobileNavigation(){
-    document.querySelector('.mobile-menu').style.display = 'none';
-    document.querySelector('body').style.overflow = 'auto';
-   
+function closeMobileNavigation() {
+  document.querySelector(".mobile-menu").style.display = "none";
+  document.querySelector("body").style.overflow = "auto";
 }
+
+document
+  .querySelector(".search-bar-icon")
+  .addEventListener("click", handleSearchBar);
+function handleSearchBar() {
+
+  if(document.querySelector(".search-field").style.display == "none"){
+    document.querySelector(".search-field").style.display = "block";
+    document  .querySelector(".search-bar-icon img").setAttribute('src', 'images/cancel.png');
+  }
+  else{
+    document.querySelector(".search-field").style.display = "none";
+    document.querySelector(".search-bar-icon img").setAttribute('src', 'images/search-icon.png');
+  }
+  
+}
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("slide");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
   setTimeout(showSlides, 2000);
 }
+document
+  .querySelector(".search-bar-icon")
+  .addEventListener("click", handleSearchBar);
+
--- JSAssignment/sortByKey.js
+++ JSAssignment/sortByKey.js
@@ -13,32 +13,20 @@ var arr = [{
     name: 'Andrew',
 }];
 
-function sortBy(array, key) {
-    var newArray = [];
-    var keysArray = []
-    for(var i = 0; i<array.length; i++){
-        keysArray.push(array[i][key]);    
-    }
-    for(var x=0;x<keysArray.length; x++){
-        for(var y = 0; y<keysArray.length; y++){
-            if(keysArray[y]>keysArray[y+1]){
-                var temp = keysArray[y];
-                keysArray[y] = keysArray[y+1];
-                keysArray[y+1] = temp
+function sortBy(arr, key) {
+    var array = arr;
+    for(var x=0;x<array.length; x++){
+        for(var y = 0; y<array.length-1; y++){
+            if(array[y][key]>array[y+1][key]){
+                var temp = array[y];
+                array[y] = array[y+1];
+                array[y+1] = temp
             }
         }
         
     }
-    for(var j=0; j<keysArray.length; j++){
-        for(var y=0; y<array.length; y++){
-            if(keysArray[j]===array[y][key]){
-                var index = y
-            }
-        }
-        newArray.push(array[index])
-    }   
-    return newArray
+    return array
 }
 
-var sorted = sortBy(arr, 'name');
+var sorted = sortBy(arr, 'id');
 console.log(sorted)
