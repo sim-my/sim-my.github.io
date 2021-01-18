ea174506547955bce16f022c2dd2a690e89d6100
sim-my <simranpanthi101@gmail.com>
2021-01-18 10:08:33 +0545

    Completion

--- JSCarousel/js/addElement.js
+++ JSCarousel/js/addElement.js
@@ -56,6 +56,9 @@ class Carousel {
     obj.transitionTime === undefined
       ? (this.transitionTime = 2000)
       : (this.transitionTime = obj.transitionTime);
+    obj.autoSlide == undefined 
+      ? (this.autoSlide) = true
+      : (this.autoSlide) = obj.autoSlide;
     this.currentIndex = 0;
     this.dotClicked = false;
   }
@@ -249,28 +252,44 @@ class Carousel {
     });
   }
 
-
-  createCarousel() {
-
-    //calling methods for creating carousel
-    this.styleCarousel();
-    this.createNextButton();
-    this.createPrevButton();
-    this.createDotsContainer();
-    this.createDots();
-    this.setImagePosition();
-    this.setActiveDot();
-
-
-    //getting next and previous buttons
-    window[`buttonNext-${this.instance}`] = document.querySelector(
-      `${this.carouselId} .button-next`
+  animateDots(){
+    let images = this.getImages();
+    window[`containerDot-${this.instance}`] = document.querySelector(
+      `${this.carouselId} .dot-container-${this.instance}`
     );
 
-    window[`buttonPrev-${this.instance}`] = document.querySelector(
-      `${this.carouselId}  .button-prev`
-    );
+    for (let i = 0, len = window[`containerDot-${this.instance}`].children.length; i < len; i++) {
+      ((index) => {
+        window[`containerDot-${this.instance}`].children[i].onclick = () => {
+          let prevIndex = this.currentIndex;
+          this.currentIndex = index;
+          let currentLeft = window[`containerDot-${this.instance}`].children[i].style.left;
+          let diff = this.currentIndex - prevIndex;
+          this.setActiveDot();
+          let count = 20;
+          images.forEach((image, j) => {
+            let imgLeft = parseInt(image.style.left);
+            let current = imgLeft;
+            let setImgPos = setInterval(() => {
+              image.style.left = imgLeft + "px";
+              if (diff >= 0) {
+                imgLeft = imgLeft - count;
+                if (imgLeft < current - this.width * diff) {
+                  clearInterval(setImgPos);
+                }
+              } else {
+                imgLeft = imgLeft + count;
+                if (imgLeft > current - this.width * diff) {
+                  clearInterval(setImgPos);
+                }
+              }
+            }, this.transitionTime / (Math.abs(currentLeft - this.width * diff) / count));
+          });
+        };
+      })(i);
+    }
   }
+
   autoAnimate() {
     // getting  images and dots container
     let images = this.getImages();
@@ -359,6 +378,40 @@ class Carousel {
       })(i);
     }
   }
+
+
+  createCarousel() {
+
+    //calling methods for creating carousel
+    this.styleCarousel();
+    this.createNextButton();
+    this.createPrevButton();
+    this.createDotsContainer();
+    this.createDots();
+    this.setImagePosition();
+    this.setActiveDot();
+
+
+    //getting next and previous buttons
+    window[`buttonNext-${this.instance}`] = document.querySelector(
+      `${this.carouselId} .button-next`
+    );
+
+
+
+    window[`buttonPrev-${this.instance}`] = document.querySelector(
+      `${this.carouselId}  .button-prev`
+    );
+
+    if(!this.autoSlide){
+      window[`buttonPrev-${this.instance}`].addEventListener('click',()=>this.animatePrev());
+      window[`buttonNext-${this.instance}`].addEventListener('click',()=>this.animateNext());
+      this.animateDots();
+    }
+    else{
+      this.autoAnimate();
+    }
+  }
 }
 
 const carousel = new Carousel({
@@ -370,12 +423,11 @@ const carousel = new Carousel({
   transitionTime: 4000,
 });
 carousel.createCarousel();
-carousel.autoAnimate();
 
 const carousel1 = new Carousel({
   carouselId: "#carousel-container-2",
   instance: 2,
-  imageCount: 4,
+  imageCount: 4
 });
 carousel1.createCarousel();
-carousel1.autoAnimate();
+
