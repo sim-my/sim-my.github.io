bda6b2afa5da2a1369c799e7b55f8ddecb6bb160
sim-my <simranpanthi101@gmail.com>
2021-01-19 16:33:45 +0545

    Created road and car animation

--- JSCarLaneGame/js/car.js
+++ JSCarLaneGame/js/car.js
@@ -2,9 +2,38 @@ const carImage = new Image();
 
 carImage.src = './images/my_car.png';
 
+
+const possibleCarLanes = [canvas.width * (11/50), canvas.width * (19/50), canvas.width * (11/20)]
+
+let current = 0;
+
 const myCarAnimate = () => {
-    c.drawImage(carImage, canvas.width/4 - 30, canvas.height * (2/3));
+   let x = possibleCarLanes[current];
+   let y =  canvas.height * (2/3);
+   c.drawImage(carImage, x, y);
+
+
     requestAnimationFrame(myCarAnimate);    
 }
+
+const keyPressHandler = (event) => {
+    if(event.keyCode === 37){
+        current --;
+        if(current < 0){
+            current = 0;
+        }
+    }
+
+    if(event.keyCode === 39){
+        current ++;
+        if(current > 2){
+            current = 2;
+        }
+    }
+}
+
+
+window.addEventListener('keydown', keyPressHandler);
+
 
 myCarAnimate();
--- JSCarLaneGame/js/road.js
+++ JSCarLaneGame/js/road.js
@@ -1,15 +1,15 @@
 let carSpeed = 10;
-let roadTop = 0;
+let roadTop = -canvas.height;
 
 const roadBg = new Image();
 
 roadBg.src = './images/road.jpg';
 const roadAnimate = () => {
 
-c.drawImage(roadBg, canvas.width/4, roadTop - canvas.height, canvas.width/2, 2 * canvas.height);
+c.drawImage(roadBg, canvas.width/4, roadTop, canvas.width/2, 2 * canvas.height);
       
     roadTop += carSpeed;  
-    if( roadTop > canvas.height ) roadTop = 0;
+    if( roadTop >= 0) roadTop = -canvas.height;
 
     requestAnimationFrame(roadAnimate);
 }    
