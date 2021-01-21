f3ac202889963e6c4f3993984cb993f806885bf5
sim-my <simranpanthi101@gmail.com>
2021-01-21 11:53:50 +0545

    check score problem

--- JSCarLaneGame/js/obstacle.js
+++ JSCarLaneGame/js/obstacle.js
@@ -115,11 +115,11 @@ const checkCollision = () => {
       }
       score = 0;
       exitMenu.style.display = "block";
+    } else if (myCarY < obstacle.y - vehicleHeight) {
+      score = score + 1;
+      console.log(score)
+      scoreEl.innerHTML = Math.trunc(score / 5);
     }
-    // else if (myCarY < obstacle.y - vehicleHeight) {
-    //   score = score + 1;
-    //   scoreEl.innerHTML = Math.trunc(score / 5);
-    // }
   });
 };
 
@@ -128,16 +128,11 @@ const animateObstacle = () => {
   generateObstacle();
   checkCollision();
   obstacleArray.forEach((obstacle) => {
-    if(obstacle.y > canvas.height){
-      onstacleArray.shift();
-      score = score + 1;
-      scoreEl.innerHTML = Math.trunc(score / 5);
-    }
     obstacle.update();
   });
-  // obstacleArray = obstacleArray.filter(
-  //   (obstacle) => obstacle.y < canvas.height
-  // );
+  obstacleArray = obstacleArray.filter(
+    (obstacle) => obstacle.y < canvas.height
+  );
   requestAnimationFrame(animateObstacle);
 };
 
