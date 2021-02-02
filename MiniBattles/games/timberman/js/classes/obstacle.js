ff2fcdee26a948634587d03ba1992b4eef034937
sim-my <simranpanthi101@gmail.com>
2021-02-02 20:00:23 +0545

    del unrelated folder

--- MiniBattles/games/timberman/js/classes/obstacle.js
+++ MiniBattles/games/timberman/js/classes/obstacle.js
@@ -1,4 +1,4 @@
-import Element from '../../../../core/js/classes/Element.js';
+import Element from '../../../../core/js/classes/element.js';
 export default class Obstacle extends Element {
   constructor(canvas, context,image, x, y, width, height, direction) {
     super(canvas, context, image, x, y, width, height)
